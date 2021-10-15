0423f6dab2b36f05aea524a464625a2d97656e18
whelfjhte79 <whelfjhte79@naver.com>
2021-10-15 16:32:18 +0900

    Fix image size

---
+++ main/java/com/example/bilnddateapp/MembershipCertificationScreen.java
@@ -0,0 +1,13 @@
+package com.example.bilnddateapp;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MembershipCertificationScreen extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_membership_certification_screen);
+    }
+}
---
+++ main/java/com/example/bilnddateapp/UserRecommendation.java
@@ -0,0 +1,13 @@
+package com.example.bilnddateapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserRecommendation extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommendation);
+    }
+}
